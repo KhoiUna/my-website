ccec57b57f77ac2c49192c902533b7ecabb64d9d
KhoiNguyen <thorwaitson@gmail.com>
2022-02-18 13:39:11 -0600

    add jquery

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,25 @@
 const header = document.querySelector("header");
+const readMoreButton = document.querySelector("#read-more");
 
 window.addEventListener("scroll", () => {
   if (document.querySelector("html").scrollTop > 0)
     return (header.style.animation = "colorFadeIn 1s forwards");
 
   return (header.style.animation = "colorFadeOut 1s forwards");
+});
+
+$().ready(() => {
+  $("#read-more").on("click", (e) => {
+    const destination = $("#introduction");
+    const scrollPosition =
+      destination.offset().top - destination.offset().top * 0.12;
+    const animationDuration = 500;
+
+    $("html").animate(
+      {
+        scrollTop: scrollPosition,
+      },
+      animationDuration
+    );
+  });
 });
