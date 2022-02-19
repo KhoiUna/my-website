a3f2b86b622b18e77ddaa7ede08d4cecd01d1964
KhoiNguyen <thorwaitson@gmail.com>
2022-02-18 19:22:55 -0600

    update header navigation

--- scripts/script.js
+++ scripts/script.js
@@ -12,47 +12,59 @@ const headerFading = () => {
   return (header.style.animation = "colorFadeOut 1s forwards");
 };
 
+const smoothScroll = (cssSelector) => {
+  const destination = $(cssSelector);
+  const scrollPosition =
+    destination.offset().top - destination.offset().top * 0.05;
+
+  $("html").animate(
+    {
+      scrollTop: scrollPosition,
+    },
+    animationDuration
+  );
+};
+
+const toggleMenu = () => {
+  if (!menuOpened) {
+    menuOpened = !menuOpened;
+    headerFading();
+
+    $("#burger-icon").fadeOut(animationDuration);
+    $("#dropdown-menu").fadeIn(animationDuration);
+    setTimeout(() => {
+      $("#close-icon").fadeIn(animationDuration);
+    }, animationDuration);
+  } else {
+    menuOpened = !menuOpened;
+    headerFading();
+
+    $("#close-icon").fadeOut(animationDuration);
+    $("#dropdown-menu").fadeOut(animationDuration);
+    setTimeout(() => {
+      $("#burger-icon").fadeIn(animationDuration);
+    }, animationDuration);
+  }
+};
+
 // Select elements
 const header = document.querySelector("header");
 const readMoreButton = document.querySelector("#read-more");
 
+// JS interactions
 window.addEventListener("scroll", headerFading);
 
 $().ready(() => {
-  // dropdown-menu
-  $("#burger-menu").click(() => {
-    if (!menuOpened) {
-      menuOpened = !menuOpened;
-      headerFading();
-
-      $("#burger-icon").fadeOut(animationDuration);
-      $("#dropdown-menu").fadeIn(animationDuration);
-      setTimeout(() => {
-        $("#close-icon").fadeIn(animationDuration);
-      }, animationDuration);
-    } else {
-      menuOpened = !menuOpened;
-      headerFading();
-
-      $("#close-icon").fadeOut(animationDuration);
-      $("#dropdown-menu").fadeOut(animationDuration);
-      setTimeout(() => {
-        $("#burger-icon").fadeIn(animationDuration);
-      }, animationDuration);
-    }
+  //nav-links
+  $(".nav-links").click((event) => {
+    const destination = event.currentTarget.attributes.href.nodeValue;
+    smoothScroll(destination);
+    toggleMenu();
   });
+
+  // dropdown-menu
+  $("#burger-menu").click(toggleMenu);
 
   // read-more button
-  $("#read-more").on("click", (e) => {
-    const destination = $("#introduction");
-    const scrollPosition =
-      destination.offset().top - destination.offset().top * 0.12;
-
-    $("html").animate(
-      {
-        scrollTop: scrollPosition,
-      },
-      animationDuration
-    );
-  });
+  $("#read-more").on("click", () => smoothScroll("#introduction"));
 });
